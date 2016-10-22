65c09c3a349e5b8ec162dbc48beaf05ef0219e0d
Jack Harvey <jack.harvey@news.co.uk>
2016-10-22 22:45:04 +0100

    Working on vertical

--- connect4/src/connect4.js
+++ connect4/src/connect4.js
@@ -1,23 +1,106 @@
- /*jshint esversion: 6 */
+/*jshint esversion: 6 */
 (function() {
     "use strict";
 })();
 
+//7 columns 6 rows
+
 var board = [];
 
-function initialiseBoard(columns, rows) {
-    for (var i = 0; i < columns; i++) {
-        board[i] = [];
-        for (var j = 0; j < rows; j++) {
-            board[i][j] = false;
+
+
+
+
+function hasWon() {
+
+}
+
+function getSubarray(startX, startY) {
+    if (startX )
+    for (var x = startX; i < startX + 4; i++) {
+
+    }
+}
+
+function hasVertical() {
+    var vertical = 'X\nX\nX\nX';
+    for (var i = 0; i < board.length; i++) {
+        var columnFlag = false;
+        for(var j = 0; j < board[0].length - 4; j++) {
+            var column = [
+                board[i][j],
+                board[i][j+1],
+                board[i][j+2],
+                board[i][j+3]
+            ];
+            if (column === vertical) {
+
+            }
         }
     }
 }
 
-function fallsTo(column) {
-    reverse(board[column]).forEach((cell) => {
-        if (cell === false) {
-            return
+function hasHorizontal() {
+    var horizontal = 'XXXX';
+}
+
+function hasDiagonal() {
+    var diagonalRight = 'XOOO\nOXOO\nOOXO\nOOOX';
+    var diagonalLeft = 'OOOX\nOOXO\nOXOO\nXOOO';
+}
+
+function getColours() {
+    var colours = [];
+    for (var i = 0; i < board.length; i++) {
+        for(var j = 0; j < board[0].length; j++) {
+            if (colours.indexOf(board[i][j]) === -1 && board[i][j] !== 'O') {
+                colours.push(board[i][j]);
+            }
         }
-    });
+    }
+    return colours;
+}
+
+function insert(column, colour) {
+    if (column > board.length) {
+        return;
+    } else {
+        for (var i = 0; i < board[column].length; i++) {
+            if (board[column][i] === 'R' || board[column][i] === 'Y') {
+                board[column][i - 1] = colour;
+                return;
+            } else if (i === board[column].length - 1) {
+                board[column][i] = colour;
+                return;
+            }
+        }
+    }
+}
+
+function instantiateBoard(columns, rows) {
+    for (var i = 0; i < columns; i++) {
+        board[i] = [];
+        for(var j = 0; j < rows; j++) {
+            board[i][j] = 'O';
+        }
+    }
+}
+
+function resetBoard() {
+    for (var i = 0; i < board.length; i++) {
+        for(var j = 0; j < board[0].length; j++) {
+            board[i][j] = 'O';
+        }
+    }
+}
+
+function printBoard() {
+    for (var row = 0; row < board[0].length; row++) {
+        var rowString = "";
+        for (var col = 0; col < board.length; col++) {
+            rowString += board[col][row];
+        }
+        console.log(rowString);
+    }
+    console.log('-------');
 }
--- connect4/test/test.js
+++ connect4/test/test.js
@@ -3,30 +3,56 @@
 })();
 
 describe('Connect 4', function() {
-    beforeEach(function() {
-        initialiseBoard(7, 6);
+    beforeAll(function() {
+        instantiateBoard(7, 6);
     });
-    describe('initialisation', function() {
-        it('works ', function() {
-            for (var i = 0; i < 7; i++) { //columns or x
-                for (var j = 0; j < 6; j++) { //rows or y
-                    expect(board[i][j]).toBe(false);
-                }
-            }
-        });
+    afterEach(function() {
+        resetBoard();
+    });
     describe('insertion', function() {
-        it('in an empty column falls to the correct row', function() {
-            expect(fallsTo(0)).toBe(6);
+        it('works on an empty column', function() {
+            insert(0, 'R');
+            expect(board[0][5]).toBe('R');
         });
-        it('3 times falls to the correct row', function() {
-            fallsTo(0);
-            fallsTo(0);
-            
-        })
+        it('accepts two tokens', function() {
+            insert(0, 'R');
+            insert(0, 'Y');
+            expect(board[0][4]).toBe('Y');
+        });
+        it('rejects an invalid column', function() {
+            insert(1000);
+            expect(board[1000]).toBe(undefined);
+        });
+        it('cannot overfill', function() {
+            for (var i = 0; i < 20; i++) {
+                insert(0, 'R');
+            }
+            expect(board[0].length).toBe(6);
+        });
     });
-        // it('can receive tokens', funtion() {
-        //     insert(0);
-        //     expect(board[0][6]).toBe(true);
-        // });
+    describe('win conditional', function() {
+        it('collects all the colours', function() {
+            insert(0, 'Y');
+            insert(0, 'R');
+            var colours = getColours();
+            expect(colours.sort()).toEqual(['R', 'Y']);
+        });
+        //diagonal right win]
+        //diagonal left win
+        //vertical win
+        //horizontal win
+        //no win
+
+        it('returns red for vertical', function() {
+            insert(0, 'R');
+            insert(0, 'R');
+            insert(0, 'R');
+            insert(0, 'R');
+            expect(hasWon()).toBe('R');
+        });
+
+        // for(var i = 0; i < 4; i++) {
+        //     insert(0, 'R');
+        // }
     });
 });
