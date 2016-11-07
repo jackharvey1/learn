d9fbfa4ca49a19109d29a644c087b5a130f94e30
Jack Harvey <jack.harvey@news.co.uk>
2016-11-07 18:54:58 +0000

    Berlin clock done. Made a webpage

--- berlin/src/berlin.js
+++ berlin/src/berlin.js
@@ -3,6 +3,38 @@
     'use strict';
 })();
 
+function updateHTML() {
+    var clock = berlinClock(new Date()
+        .toTimeString()
+        .substring(0,8)
+    ).split('');
+
+    //seconds lamp
+    for (var c = 0; c < 24; c++) {
+        var cell = document.getElementById(c);
+        cell.className = cell.className.replace(
+            /(O|Y|R)/g, ''
+        );
+        cell.className += clock[c];
+    }
+}
+
+function berlinClock(time) {
+    var components = time.split(':');
+    var h = components[0];
+    var m = components[1];
+    var s = components[2];
+    return seconds(s) +
+        fiveHours(h) +
+        singleHours(h) +
+        fiveMinutes(m) +
+        singleMinutes(m);
+}
+
+function seconds(s) {
+    return s % 2 === 0 ? 'Y' : 'O';
+}
+
 function fiveMinutes(minutes) {
     var row = "";
     for (var m = 5; m < 60; m+=5) {
@@ -19,7 +51,19 @@ function fiveMinutes(minutes) {
     return row;
 }
 
-function singles(number, colour) {
+function singleMinutes(minutes) {
+    return fourLights(minutes, 'Y');
+}
+
+function fiveHours(hours) {
+    return fourLights(Math.floor(hours/5), 'R');
+}
+
+function singleHours(hours) {
+    return fourLights(Math.floor(hours), 'R');
+}
+
+function fourLights(number, colour) {
     number %= 5;
     var row = "";
     for (var y = 0; y < number; y++) {
--- berlin/test/test.js
+++ berlin/test/test.js
@@ -1,3 +1,4 @@
+/* jshint esversion:6*/
 (function () {
     'use strict';
 })();
@@ -5,56 +6,95 @@
 describe('Berlin clock', function () {
     describe('single minute row', function() {
         it('should display zero minutes', function () {
-            expect(singles('00', 'Y')).toBe('OOOO');
+            expect(singleMinutes(0)).toBe('OOOO');
         });
         it('should display one minute', function() {
-            expect(singles('01', 'Y')).toBe('YOOO');
+            expect(singleMinutes(1)).toBe('YOOO');
         });
         it('should display two minutes', function() {
-            expect(singles('32', 'Y')).toBe('YYOO');
+            expect(singleMinutes(32)).toBe('YYOO');
         });
         it('should display three minutes', function() {
-            expect(singles('33', 'Y')).toBe('YYYO');
+            expect(singleMinutes(33)).toBe('YYYO');
         });
         it('should display four minutes', function() {
-            expect(singles('34', 'Y')).toBe('YYYY');
+            expect(singleMinutes(34)).toBe('YYYY');
         });
         it('should display five minutes', function() {
-            expect(singles('35', 'Y')).toBe('OOOO');
+            expect(singleMinutes(35)).toBe('OOOO');
         });
     });
     describe('five minute row', function() {
         it('displays no lights', function() {
-            expect(fiveMinutes('00')).toEqual('OOOOOOOOOOO');
+            expect(fiveMinutes(0)).toEqual('OOOOOOOOOOO');
         });
         it('displays all lights', function() {
-            expect(fiveMinutes('59')).toEqual('YYRYYRYYRYY');
+            expect(fiveMinutes(59)).toEqual('YYRYYRYYRYY');
         });
         it('displays three lights', function() {
-            expect(fiveMinutes('15')).toEqual('YYROOOOOOOO');
+            expect(fiveMinutes(15)).toEqual('YYROOOOOOOO');
         });
         it('displays five lights', function() {
-            expect(fiveMinutes('25')).toEqual('YYRYYOOOOOO');
+            expect(fiveMinutes(25)).toEqual('YYRYYOOOOOO');
         });
         it('displays six lights', function() {
-            expect(fiveMinutes('30')).toEqual('YYRYYROOOOO');
+            expect(fiveMinutes(30)).toEqual('YYRYYROOOOO');
         });
     });
     describe('single hours row', function() {
         it('displays no lights', function() {
-            expect(singles('00', 'R')).toEqual('OOOO');
+            expect(singleHours(0, 'R')).toEqual('OOOO');
         });
         it('displays one lights', function() {
-            expect(singles('11', 'R')).toEqual('ROOO');
+            expect(singleHours(11, 'R')).toEqual('ROOO');
         });
         it('displays two lights', function() {
-            expect(singles('22', 'R')).toEqual('RROO');
+            expect(singleHours(22, 'R')).toEqual('RROO');
         });
         it('dislays three lights', function() {
-            expect(singles('03', 'R')).toEqual('RRRO');
+            expect(singleHours(3, 'R')).toEqual('RRRO');
         });
         it('displays four lights', function() {
-            expect(singles('14', 'R')).toEqual('RRRR');
+            expect(singleHours(14, 'R')).toEqual('RRRR');
+        });
+    });
+    describe('five hours row', function() {
+        it('displays no lights', function() {
+            expect(fiveHours(0)).toEqual('OOOO');
+        });
+        it('displays one light', function() {
+            expect(fiveHours(6)).toEqual('ROOO');
+        });
+        it('displays two lights', function() {
+            expect(fiveHours(11)).toEqual('RROO');
+        });
+        it('displays three lights', function() {
+            expect(fiveHours(16)).toEqual('RRRO');
+        });
+        it('displays four lights', function() {
+            expect(fiveHours(23)).toEqual('RRRR');
+        });
+    });
+    describe('the seconds lamp', function() {
+        it('shows a light when the time is even', function() {
+            expect(seconds(0)).toEqual('Y');
+        });
+        it('shows a light when the time is even', function() {
+            expect(seconds(53)).toEqual('O');
+        });
+    });
+    describe('berlin clock', function() {
+        it('is correct for 0:0:0', function() {
+            expect(berlinClock('0:0:0')).toEqual('YOOOOOOOOOOOOOOOOOOOOOOO');
+        });
+        it('is correct for 23:59:59', function() {
+            expect(berlinClock('23:59:59')).toEqual('ORRRRRRROYYRYYRYYRYYYYYY');
+        });
+        it('is correct for 16:50:6', function() {
+            expect(berlinClock('16:50:6')).toEqual('YRRROROOOYYRYYRYYRYOOOOO');
+        });
+        it('is correct for 11:37:1', function() {
+            expect(berlinClock('11:37:1')).toEqual('ORROOROOOYYRYYRYOOOOYYOO');
         });
     });
 });
